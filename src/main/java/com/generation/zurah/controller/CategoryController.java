b70b62e4afdcce55ce1718439865f53a805b0096
Israel Oliveira <israeloliveiracontact@gmail.com>
2024-07-11 11:54:13 -0300

    feat: methods list all and post

---
+++ src/main/java/com/generation/zurah/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package com.generation.zurah.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.zurah.model.Category;
+import com.generation.zurah.repository.CategoryRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+	
+	@Autowired
+	public CategoryRepository categoryRepository;
+	
+	public ResponseEntity<List<Category>> getAll() {
+		return ResponseEntity.ok(categoryRepository.findAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> post(@Valid @RequestBody Category category) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoryRepository.save(category));
+	}
+	
+	
+
+}
---
+++ src/main/java/com/generation/zurah/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.generation.zurah.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.zurah.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	
+
+}

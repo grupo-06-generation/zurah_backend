eb88723619551ca0aa11876203c2900e094f1e8d
FShinoda <fernanda.shinoda@hotmail.com>
2024-07-11 14:24:32 -0300

    feat: delete method add

--- src/main/java/com/generation/zurah/controller/CategoryController.java
+++ src/main/java/com/generation/zurah/controller/CategoryController.java
@@ -1,15 +1,20 @@
 package com.generation.zurah.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.zurah.model.Category;
 import com.generation.zurah.repository.CategoryRepository;
@@ -37,6 +42,17 @@ public class CategoryController {
 		return categoryRepository.findById(category.getId())
 				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(categoryRepository.save(category)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		if(category.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		categoryRepository.deleteById(id);
 	}
 
 }

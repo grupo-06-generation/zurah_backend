3154241876c3aa22bf131cbed00f986d662340c6
maardgs <mariliarodrigues.94@gmail.com>
2024-07-12 12:22:52 -0300

    Inclusão da classe Produto

--- src/main/java/com/generation/zurah/model/Category.java
+++ src/main/java/com/generation/zurah/model/Category.java
@@ -1,9 +1,16 @@
 package com.generation.zurah.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,6 +29,10 @@ public class Category {
 
 	@Size(min = 5, max = 255, message = "The description attribute must contain a minimum of 5 and a maximum of 255 characters8")
 	private String description;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "category", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("category")
+	private List<Product> product;
 
 	public Long getId() {
 		return id;
@@ -47,4 +58,13 @@ public class Category {
 		this.description = description;
 	}
 
+	public List<Product> getProduct() {
+		return product;
+	}
+
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
+
+	
 }
--- src/main/java/com/generation/zurah/model/Product.java
+++ src/main/java/com/generation/zurah/model/Product.java
@@ -1,10 +1,126 @@
 package com.generation.zurah.model;
 
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_product")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 3, max = 255, message = "O nome precisa ter no mínimo 3 caracteres e no máximo 255 caracteres")
+	private String name;
+
+	@NotBlank(message = "O atributo é obrigatório e deve seguir a ordem: mês-dia-ano")
+	private String expire;
+
+	@NotNull(message = "O atributo preço obrigatório")
+	@Column(precision = 6, scale = 2)
+	private BigDecimal price;
+
+	@NotNull(message = "O atributo quantidade é obrigatório")
+	private int quantity;
+
+	@Size(max = 1000, message = "O link da foto precisa ter no máximo 1000 caracteres")
+	private String photo;
+
+	@Size(max = 255, message = "O atributo descrição precisa ter no máximo 255 caracteres")
+	private String description;
+
+	@Size(max = 255, message = "O atributo região precisa ter no máximo 255 caracteres")
+	private String region;
+
+	@ManyToOne
+	@JsonIgnoreProperties("product")
+	private Category category;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getExpire() {
+		return expire;
+	}
+
+	public void setExpire(String expire) {
+		this.expire = expire;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	
 }

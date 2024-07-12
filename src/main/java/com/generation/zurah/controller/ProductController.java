3304b47354e48c1d02f891f1645321b1b1864aec
bellaespositorei <contato.isabellaesposito@gmail.com>
2024-07-12 14:07:06 -0300

    :sparkles: feat: Crie o metodo getAll e getById

--- src/main/java/com/generation/zurah/controller/ProductController.java
+++ src/main/java/com/generation/zurah/controller/ProductController.java
@@ -1,13 +1,37 @@
 package com.generation.zurah.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.zurah.model.Product;
+import com.generation.zurah.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/product")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductController {
 
-
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> getAll(){
+		return ResponseEntity.ok(productRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getById(@PathVariable Long id){
+		return productRepository.findById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
 }

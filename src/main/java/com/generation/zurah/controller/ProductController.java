a929d4694114fe1d42bce7915c6c5314c363a425
Julia <jrhuliafy@gmail.com>
2024-07-12 14:40:01 -0300

    feat: Crie método de busca específico e método post

--- src/main/java/com/generation/zurah/controller/ProductController.java
+++ src/main/java/com/generation/zurah/controller/ProductController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.zurah.model.Product;
 import com.generation.zurah.repository.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
@@ -21,17 +25,28 @@ public class ProductController {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Product>> getAll(){
 		return ResponseEntity.ok(productRepository.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Product> getById(@PathVariable Long id){
 		return productRepository.findById(id)
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
-	
+
+	@GetMapping("/name/{name}")
+	public ResponseEntity <List<Product>> getByName(@PathVariable String name){
+		return ResponseEntity.ok(productRepository.findAllByNameContainingIgnoreCase(name));
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Product> post(@Valid @RequestBody Product product){
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(product));
+
+	}
 }
--- src/main/java/com/generation/zurah/repository/ProductRepository.java
+++ src/main/java/com/generation/zurah/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.generation.zurah.repository;
 
-import com.generation.zurah.model.Product;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.generation.zurah.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	public List<Product> findAllByNameContainingIgnoreCase(@Param("name") String name);
 }
